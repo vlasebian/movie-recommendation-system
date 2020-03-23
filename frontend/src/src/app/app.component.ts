bc67edba7a4946ad6918774a808f0a96c3495d1e
Vlad Vitan <vlad.vitan@gmail.com>
2020-03-23 19:14:57 +0200

    fixed some typos

--- frontend/src/src/app/app.component.ts
+++ frontend/src/src/app/app.component.ts
@@ -8,7 +8,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Movify';
+  title = 'Recommendr.';
   // currentUser: IUser;
 
   // constructor(
